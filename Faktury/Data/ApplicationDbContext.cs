47a5035288ef3de971e787b10a7af4bbd88cdeef
Devilek66 <Aleksanderp1993@gmail.com>
2019-05-02 23:39:56 +0200

    Dodaj pliki projektów.

---
+++ Faktury/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Faktury.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
